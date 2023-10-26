4e6451af92705079944228c485788bdd853956af
Brighton Dube <BrightonDube@users.noreply.github.com>
2023-10-26 18:27:02 +0200

    products added

--- src/templates/ServicesPage.js
+++ src/templates/ServicesPage.js
@@ -38,6 +38,34 @@ export const ServicesPageTemplate = ({
 
     <section className="section">
       <div className="container">
+        <h1>Buy Crystal Rocks From Us</h1>
+        <h>We have these beautiful Gemstones for sale</h>
+
+        <Gallery images={gallery} />
+      </div>
+    </section>
+
+    <section className="section">
+      <div className="container">
+        <Gallery images={gallery} />
+        <div
+          style={{
+            paddingTop: '80px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%'
+          }}
+        >
+          <h4>
+            Call or Whatsapp <a href="tel:+27615895614">061 589 5614</a> Today
+            for more information!
+          </h4>
+        </div>
+      </div>
+    </section>
+    <section className="section">
+      <div className="container">
         <Accordion items={accordion} />
       </div>
     </section>
